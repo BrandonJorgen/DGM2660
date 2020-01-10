34ae729cb10902a7e3c2e889446ccfeb47ff5ca5
Brandon <brandonjorgen@gmail.com>
2020-01-09 18:45:17 -0700

    X/Z Axis Movement

--- Unity Project/Assets/Scripts/CharacterControllerBehavior.cs
+++ Unity Project/Assets/Scripts/CharacterControllerBehavior.cs
@@ -4,9 +4,20 @@
 public class CharacterControllerBehavior : MonoBehaviour
 {
     private CharacterController controller;
+    private Vector3 position;
+
+    public float moveSpeed, gravity = -9.81f;
     
     void Start()
     {
         controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        position.x = moveSpeed * Input.GetAxis("Horizontal");
+        position.z = moveSpeed * Input.GetAxis("Vertical");
+        
+        controller.Move(position * Time.deltaTime);
     }
 }
