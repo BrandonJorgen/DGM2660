eb1b6c6540417c66d8142c5268b1670fb262a3ac
Brandon <brandonjorgen@gmail.com>
2020-01-09 17:38:38 -0700

    Unity 2019.1.14f1 Project Added
    also added Jetbrains Rider plugins

---
+++ Unity Project/Assets/Scripts/CharacterControllerBehavior.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterControllerBehavior : MonoBehaviour
+{
+    private CharacterController controller;
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+}
