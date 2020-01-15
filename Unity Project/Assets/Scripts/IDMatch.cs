0fde20022b27f1225443e0ede1780c9ef5449ee8
Brandon <brandonjorgen@gmail.com>
2020-01-15 07:18:15 -0700

    Setting up Anthony's in-class work

---
+++ Unity Project/Assets/Scripts/IDMatch.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class IDMatch : MonoBehaviour
+{
+    public IDName nameIDObj;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        var nameIdObj = other.GetComponent<IDName>().nameIDObj;
+        if (nameIdObj == null) return;
+        var otherNameId = nameIdObj;
+        if (nameIDObj == otherNameId)
+        {
+            //do work
+        }
+    }
+}
