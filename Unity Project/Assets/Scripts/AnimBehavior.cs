7e1a356443a9bcd62e67e62eabf0da441f2b876a
Brandon <brandonjorgen@gmail.com>
2020-01-26 12:01:47 -0700

    Minor scripting
    Key presses to trigger animation parameters

--- Unity Project/Assets/Scripts/AnimBehavior.cs
+++ Unity Project/Assets/Scripts/AnimBehavior.cs
@@ -3,7 +3,7 @@ using UnityEngine.Events;
 
 public class AnimBehavior : StateMachineBehaviour
 {
-    public UnityEvent stateEnterEvent;
+    public UnityEvent stateEnterEvent, stateUpdateEvent, stateExitEvent;
     
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -13,26 +13,14 @@ public class AnimBehavior : StateMachineBehaviour
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        stateUpdateEvent.Invoke();
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        stateExitEvent.Invoke();
+    }
 }
---
+++ Unity Project/Assets/Scripts/AnimTriggerBehavior.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Animator))]
+public class AnimTriggerBehavior : MonoBehaviour
+{
+    private Animator animator;
+
+    public UnityEvent wKeyEvent, sKeyEvent, aKeyEvent, dKeyEvent, noKeyEvent;
+    
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.inputString == "")
+        {
+            noKeyEvent.Invoke();
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                wKeyEvent.Invoke();
+            }
+    
+            if (Input.GetKey(KeyCode.S))
+            {
+                sKeyEvent.Invoke();
+            }
+            
+            if (Input.GetKey(KeyCode.A))
+            {
+                aKeyEvent.Invoke();
+            }
+            
+            if (Input.GetKey(KeyCode.D))
+            {
+                dKeyEvent.Invoke();
+            }
+        }
+    }
+}
